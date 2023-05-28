c2afd2e8b7ea1c39474607a471e7e26993f44a27
NadeemTalaat <nadeem.talaat@gmail.com>
2023-05-28 19:06:17 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+// Import and require mysql2
+const mysql = require("mysql2");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "127.0.0.1",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "",
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
